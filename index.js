31af9cee5bc1a7d787c4641cc010530bbbac6fc1
iamanojsonth <manojns3007@gmail.com>
2023-03-17 20:45:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+    const head = React.createElement('h1', {}, 'Namaste Everyone!');
+    // console.log(head);
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(head);
